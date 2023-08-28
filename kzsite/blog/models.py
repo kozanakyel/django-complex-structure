64606e786209f940ec2e065f4dece030bd0828e6
kozanakyel <kozanakyel@gmail.com>
2023-08-28 18:50:13 +0300

    edited pagination

--- kzsite/blog/models.py
+++ kzsite/blog/models.py
@@ -1,8 +1,14 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 # Create your models here.
+
+
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(status=Post.Status.PUBLISHED)
 
 
 class Post(models.Model):
@@ -11,7 +17,7 @@ class Post(models.Model):
         PUBLISHED = "PB", "Published"
 
     title = models.CharField(max_length=250)
-    slug = models.SlugField(max_length=250)
+    slug = models.SlugField(max_length=250, unique_for_date="publish")
     # for CASCADE if the user deleted, then all the blogs for user deleted
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="blog_post")
     body = models.TextField()
@@ -21,6 +27,8 @@ class Post(models.Model):
     status = models.CharField(
         max_length=2, choices=Status.choices, default=Status.DRAFT
     )
+    objects = models.Manager()
+    published = PublishedManager()
 
     class Meta:
         ordering = ["-publish"]  # it is good or not  i think!!1
@@ -30,3 +38,9 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse(
+            "blog:post_detail",
+            args=[self.publish.year, self.publish.month, self.publish.day, self.slug],
+        )
