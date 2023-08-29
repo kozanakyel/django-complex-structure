86ce03d464dc9b1e955a4c16a4d5c251096503c9
kozanakyel <kozanakyel@gmail.com>
2023-08-29 20:26:55 +0300

    added sitemaps and markdowns

--- kzsite/kzsite/settings.py
+++ kzsite/kzsite/settings.py
@@ -81,8 +81,11 @@ WSGI_APPLICATION = "kzsite.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        # "NAME": BASE_DIR / "db.sqlite3",
+        "NAME": "blog",
+        "USER": "postgres",
+        "PASSWORD": "postgres",
     }
 }
 
